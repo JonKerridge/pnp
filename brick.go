9c817bf5fa87a929bdb940e296bac273f5fdd462
JonKerridge <jmandsdk@btinternet.com>
2014-05-12 18:16:57 +0100

    Modified in the light that wait groups work
    We can now use the concurrent version of PlusP as it now appears to
    work.  Thus there have been consequent changes in Minus and Tabulate

--- brick.go
+++ brick.go
@@ -6,7 +6,7 @@ which at times may seem surprising.
 
 Each func has a number of channels with a consistent naming convetion.
 Channels that are used to receive values are named in.
-Channels that are used to send values are named out. 
+Channels that are used to send values are named out.
 
 Multiple channels are differentiated by appending a digit to the name.
 
@@ -30,7 +30,7 @@ import (
 )
 
 /*
-Prefix outputs the initial value on the out channel and then sends 
+Prefix outputs the initial value on the out channel and then sends
 all values received on the in channel to the out channel unaltered.
 */
 func Prefix(in chan int,
@@ -41,8 +41,9 @@ func Prefix(in chan int,
 		out <- (<-in)
 	}
 }
+
 /*
-ResetPrefix outputs the initial value on the out channel and then sends 
+ResetPrefix outputs the initial value on the out channel and then sends
 all values received on the in channel to the out channel unaltered.
 Any input on the channel reset overwrites the next input from the in
 channel.
@@ -55,11 +56,11 @@ func ResetPrefix(in chan int,
 	out <- initial
 	for {
 		select {
-			case r = <- reset:
-				<-in
-				out <- r
-			case v = <-in:
-				out <- v
+		case r = <-reset:
+			<-in
+			out <- r
+		case v = <-in:
+			out <- v
 		}
 	}
 }
@@ -77,20 +78,21 @@ func Plus1(in chan int,
 
 /*
 RecvValue receives a single value from the in channel which it stores in
-the variable valuePtr.  RecvValue does not contain a for {} loop; it terminates once 
-the value has been read from the in channel
-See its use in PlusP
+the variable valuePtr.  RecvValue does not contain a for {} loop;
+it terminates once the value has been read from the in channel
+See its use in PlusP and other functions
 */
-func RecvValue (valuePtr *int,
-	in chan int, 
+func RecvValue(valuePtr *int,
+	in chan int,
 	wg *sync.WaitGroup) {
 
 	*valuePtr = <-in
 	wg.Done()
-}	
+}
+
 /*
 SendValue sends the single value to the out channel.  SendValue does not
-contain a for {} loop; it terminates once the value has been sent.  
+contain a for {} loop; it terminates once the value has been sent.
 See its use in Copy2
 */
 func SendValue(value int,
@@ -99,9 +101,9 @@ func SendValue(value int,
 }
 
 /*
-Copy2 receives a value from its in channel, which it then sends to 
-each of the channels out1 and out2.  The sending of the values is achieved 
-using calls to go SendValue
+Copy2 receives a value from its in channel, which it then sends to
+each of the channels out1 and out2.  The sending of the values is achieved
+using calls to go SendValue and thus happen concurrently
 */
 func Copy2(in chan int,
 	out1 chan int,
@@ -114,84 +116,99 @@ func Copy2(in chan int,
 }
 
 /*
-Tail receives a value from the in channel which it then ignores, therafter
-it send all received values on the in channel to the out channel unaltered.
+CopyN receives a value on its in channel and then writes the same value
+concurrently to all the channels in the slice of channels out.
 */
-func Tail (in chan int,
-	out chan int) {
-	v := <- in	// receive and throw away 
+func CopyN(in chan int,
+	out []chan int) {
+	l := len(out)
+	var i, v int = 0, 0
 	for {
-		v = <- in
+		i = 0
+		v = <-in
+		for i < l {
+			go SendValue(v, out[i])
+			i = i + 1
+		}
+	}
+}
+
+/*
+Tail receives a value from the in channel which it then ignores, therafter
+it sends all received values on the in channel to the out channel unaltered.
+*/
+func Tail(in chan int,
+	out chan int) {
+	v := <-in // receive and throw away
+	for {
+		v = <-in
 		out <- v
 	}
 }
 
-
 /*
-PlusP receives two values one from each of its in channels in1 and in2 
+PlusP receives two values one from each of its in channels in1 and in2
 and it then sends the sum of the values to the out channel.
 It uses go routine RecvValue to read the values
 regardless of the order in which they arrive.
 
-NOTE the sync/WaitGroup is required 
+NOTE the sync/WaitGroup is required
 because go routines have no concept of
 coordinated termination as say happens in occam or JCSP
 and also in Unix as in fork / join
 */
-func PlusP (in1 chan int,
+func PlusP(in1 chan int,
 	in2 chan int,
-	out chan int ) {
+	out chan int) {
 	var v1, v2 int = 0, 0
 	var wg sync.WaitGroup
 	for {
 		wg.Add(2)
-		go RecvValue (&v1, in1, &wg)
-		go RecvValue (&v2, in2, &wg)
+		go RecvValue(&v1, in1, &wg)
+		go RecvValue(&v2, in2, &wg)
 		wg.Wait()
 		out <- v1 + v2
 	}
 }
 
 /*
-Minus receives two values one from each of its in channels in1 and in2 
-and it then sends the difference of the values to the out channel.  
+Minus receives two values one from each of its in channels in1 and in2
+concurrently and it then sends the difference of the values to the out channel.
 The value from in2 is subtracted from that obtained from in1.
 The contained select statement ensures that a pair of values are received,
 one from each channel, regardless of the order in which they arrive.
 */
-func Minus (in1 chan int,
+func Minus(in1 chan int,
 	in2 chan int,
-	out chan int ) {
+	out chan int) {
 	var v1, v2 int = 0, 0
+	var wg sync.WaitGroup
 	for {
-		//read in1 and in2 in parallel
-		select {
-			case v1 = <-in1:
-				v2 = <-in2
-			case v2 = <-in2:
-				v1 = <- in1
-		}
+		wg.Add(2)
+		go RecvValue(&v1, in1, &wg)
+		go RecvValue(&v2, in2, &wg)
+		wg.Wait()
 		out <- v1 - v2
 	}
 }
 
 /*
-Plus receives two values one from each of its in channels in1 and in2 
-and it then sends the sum of the values to the out channel.  
+Plus receives two values one from each of its in channels in1 and in2
+and it then sends the sum of the values to the out channel.
 The contained select statement ensures that a pair of values are received,
 one from each channel, regardless of the order in which they arrive.
 */
-func Plus (in1 chan int,
+func Plus(in1 chan int,
 	in2 chan int,
-	out chan int ) {
+	out chan int) {
 	var v1, v2 int = 0, 0
 	for {
 		//read in1 and in2 in pseudo parallel
 		select {
-			case v1 = <-in1:
-				v2 = <-in2
-			case v2 = <-in2:
-				v1 = <- in1
+		case v1 = <-in1:
+			v2 = <-in2
+		case v2 = <-in2:
+			v1 = <-in1
 		}
 		out <- v1 + v2
 	}
@@ -207,35 +224,37 @@ release of the scheduler (15th August 2013)
 func Delay(in chan int,
 	out chan int,
 	seconds int64) {
-	delay := time.Second * time.Duration(seconds) 
+	delay := time.Second * time.Duration(seconds)
+	var v int = 0
 	for {
-		out <- (<-in)
+		v = <-in
 		time.Sleep(delay) //should work in go1.2
+		out <- v
 	}
 }
 
 /*
-Consumer receives values from its in channel, which 
+Consumer receives values from its in channel, which
 are then printed on the console.
 
 It currently does not work correctly due to scheduler problems see Delay.
 */
-func Consumer (in chan int) {
+func Consumer(in chan int) {
 	var v int = 0
-	v = <- in
+	v = <-in
 	for v >= 0 {
 		fmt.Printf("%v\n", v)
-		v = <- in
+		v = <-in
 	}
 }
 
 /*
-Producer sends the values 0 .. iterations upwards in steps of 1 to 
-the out channel. It then send the value -1 to the out channel which 
+Producer sends the values 0 .. iterations upwards in steps of 1 to
+the out channel. It then send the value -1 to the out channel which
 can be used as a terminating value
 */
-func Producer (out chan int,
-	iterations int){
+func Producer(out chan int,
+	iterations int) {
 	var v int = 0
 	for v < iterations {
 		out <- v
@@ -245,28 +264,45 @@ func Producer (out chan int,
 }
 
 /*
-Tabulate receives inputs from an array of in channels, which it tabulates 
+Tabulate receives inputs from an array of in channels, which it tabulates
 into a single line of text which is sent as a string to the out channel.
 
-A value has to be received from every input channel before any output is generated.
-It would be nice to do this concurrently but we have seen in PlusP that this
-capability is not present.
+A value has to be received from every input channel before any output is
+generated. This input is undertaken concurrently.
 */
-func Tabulate (in[] chan int,
-	out chan string){
+func Tabulate(in []chan int,
+	out chan string) {
 	var s string
-	var v int = 0
 	var i int
+	var wg sync.WaitGroup
 	l := len(in)
+	var values = make([]int, l)
 	for {
 		s = ""
 		i = 0
+		wg.Add(l)
 		for i < l {
-			v = <- in[i]
-			s = s + fmt.Sprintf("\t%v", v)
+			go RecvValue(&values[i], in[i], &wg)
+			i = i + 1
+		}
+		wg.Wait()
+		for i < l {
+			s = s + fmt.Sprintf("\t%v", values[i])
 			i = i + 1
 		}
 		out <- s
+	}
+}
+
+/*
+Display takes a string input from the in channel and outputs its values
+to the console.
+*/
+func Display(in chan string) {
+	var s string
+	for {
+		s = <-in
+		fmt.Printf("%v\n", s)
 	}
 }
 
@@ -292,37 +328,37 @@ Queue operates as a component in a Client-Server network.
 The Queue behaves as a pure server in that it responds to receives
 on its input channels in finite time.
 
-The example exerciseQueue shows how Queue can be used in 
+The example exerciseQueue shows how Queue can be used in
 conjunction with Producer and Prompt, which both act as pure clients.
 */
-func Queue ( put chan int,
+func Queue(put chan int,
 	get chan int,
 	out chan int,
-	elements int ) {
-	var front, rear, count int = 0,0,0
+	elements int) {
+	var front, rear, count int = 0, 0, 0
 	var data = make([]int, elements)
 	for {
 		if count == 0 {
-		// can only put elements into queue
-			data[front] = <- put
+			// can only put elements into queue
+			data[front] = <-put
 			front = (front + 1) % elements
 			count = count + 1
 		} else if count == elements {
-		// can only get data from queue
-			<- get
+			// can only get data from queue
+			<-get
 			out <- data[rear]
-			rear = (rear + 1) % elements 
+			rear = (rear + 1) % elements
 			count = count - 1
 		} else {
-		// can either do a get or put
+			// can either do a get or put
 			select {
-			case data[front] = <- put:
+			case data[front] = <-put:
 				front = (front + 1) % elements
 				count = count + 1
-			case <- get:
+			case <-get:
 				out <- data[rear]
-				rear = (rear + 1) % elements 
-				count = count - 1				
+				rear = (rear + 1) % elements
+				count = count - 1
 			}
 		}
 	}
@@ -337,11 +373,11 @@ It can be used in many networks where data is being streamed and can be used to
 avoid deadlock and livelock or to provide a break in the data stream so that different parts
 of the stream can run at different rates.
 */
-func Prompt ( get chan int,
+func Prompt(get chan int,
 	recv chan int,
-	out chan int ) {
+	out chan int) {
 	for {
 		get <- 1
-		out <-  <- recv	
+		out <- <-recv
 	}
-}	
+}
